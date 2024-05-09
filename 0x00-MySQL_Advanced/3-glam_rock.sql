45b9c0a52cf0acb4050aa7106461143566832ce2
Mohamedswahed <mohamedswahed@gmail.com>
2024-05-09 12:45:49 -0700

    glam_rock

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,9 +1,6 @@
-SELECT band_name, 
-    IFNULL(
-        TIMESTAMPDIFF(YEAR, formed, 
-            CASE WHEN split = 'Present' THEN '2022-01-01' ELSE split END
-        ), 0) AS lifespan
-FROM metal_bands
-WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC;
-
+-- Lists all bands with Glam rock as their main style, ranked by their longevity.
+-- SELECT band_name, (IFNULL(split, YEAR(CURRENT_DATE())) - formed) AS lifespan
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
