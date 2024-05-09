61f2942ee4b1368e324801e591a40e521bf4e9e8
Mohamedswahed <mohamedswahed@gmail.com>
2024-05-09 12:44:48 -0700

    glam_rock

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,9 @@
--- a SQL script that lists all bands with Glam rock as their main style
-SELECT band_name, IFNULL(split, 2020) - formed AS lifespan
+SELECT band_name, 
+    IFNULL(
+        TIMESTAMPDIFF(YEAR, formed, 
+            CASE WHEN split = 'Present' THEN '2022-01-01' ELSE split END
+        ), 0) AS lifespan
 FROM metal_bands
-WHERE FIND_IN_SET("Glam rock", style)
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
+
